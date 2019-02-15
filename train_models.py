5a40c3924f2d12fab4d82275d38e6a51e03a4972
Josh Feldman <joshfeldman95@gmail.com>
2019-02-15 11:36:26 -0500

    fixed bugs in kaggle submission

--- train_models.py
+++ train_models.py
@@ -6,13 +6,13 @@ import numpy as np
 
 from namedtensor import ntorch, NamedTensor
 
-def make_kaggle_submission(model, TEXT, path_to_data = "./data/input.txt", device = 'cpu'):
-    kaggle_input = load_kaggle_data(path_to_data, TEXT, device)
+def make_kaggle_submission(model, TEXT, path_to_data = "./data/", device = 'cpu'):
+    kaggle_input = load_kaggle_data(path_to_data+"/input.txt", TEXT, device)
     pred = model.predict(kaggle_input, predict_last=True)
 
     _, top20 = pred[{'seqlen':-1}].values.topk(20, dim = 1)
 
-    with open("./data/sample.txt", "w") as fout:
+    with open(path_to_data+"/sample.txt", "w") as fout:
         print("id,word", file=fout)
         for i, text in enumerate(top20, 1):
             predictions = [TEXT.vocab.itos[word] for word in text]
